31151c26127a5ab7a936ec8f0858351459ecea7e
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-06 10:04:41 -0700

    Initial Commit

---
+++ ISYS565-Assignment02/ISYS565_Assignment02App.swift
@@ -0,0 +1,17 @@
+//
+//  ISYS565_Assignment02App.swift
+//  ISYS565-Assignment02
+//
+//  Created by JPL-ST-SPRING2022 on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ISYS565_Assignment02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ISYS565-Assignment02UITests/ISYS565_Assignment02UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ISYS565_Assignment02UITestsLaunchTests.swift
+//  ISYS565-Assignment02UITests
+//
+//  Created by JPL-ST-SPRING2022 on 10/6/22.
+//
+
+import XCTest
+
+class ISYS565_Assignment02UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
